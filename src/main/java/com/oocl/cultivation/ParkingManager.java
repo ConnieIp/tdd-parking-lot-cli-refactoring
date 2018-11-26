d22f1ac7c1df66c04ae0eab8fa06e69b891bb0b0
Connie Ip <connie.ip@cargosmart.com>
2018-11-26 23:32:02 +0800

    [Connie] fix: remove inheritance for ParkingManager

--- src/main/java/com/oocl/cultivation/ParkingManager.java
+++ src/main/java/com/oocl/cultivation/ParkingManager.java
@@ -2,11 +2,13 @@ package com.oocl.cultivation;
 
 import java.util.ArrayList;
 
-public class ParkingManager extends ParkingBoy{
+public class ParkingManager{
     private final ArrayList<ParkingBoy> parkingBoys;
+    private final ArrayList<ParkingLot> parkingLots;
+    private String lastErrorMessage;
 
     public ParkingManager(ArrayList<ParkingLot> parkingLots, ArrayList<ParkingBoy> parkingBoys) {
-        super(parkingLots);
+        this.parkingLots = parkingLots;
         this.parkingBoys = parkingBoys;
     }
 
@@ -24,5 +26,48 @@ public class ParkingManager extends ParkingBoy{
 
     private void getLastErrorMessageFromParkingBoy(ParkingBoy parkingBoy){
         lastErrorMessage = parkingBoy.getLastErrorMessage();
+    }
+
+    public ParkingLot getAvailableParkingLot(){
+        for(ParkingLot parkingLot:parkingLots){
+            if(parkingLot.getAvailableParkingPosition()>0){
+                return parkingLot;
+            }
+        }
+        return null;
+    }
+    public ParkingTicket park(Car car) {
+        ParkingLot parkingLot=getAvailableParkingLot();
+        if(parkingLot==null){
+            lastErrorMessage = "The parking lot is full.";
+            return null;
+        }else {
+            ParkingTicket parkingTicket = parkingLot.addCar(car);
+            if (parkingTicket == null) {
+                lastErrorMessage = "The parking lot is full.";
+            }
+            return parkingTicket;
+        }
+    }
+
+    public Car fetch(ParkingTicket ticket) {
+        if(ticket==null){
+            lastErrorMessage="Please provide your parking ticket.";
+            return null;
+        }
+        ParkingLot parkingLot=ticket.getParkingLot();
+        if(!parkingLots.contains(parkingLot)){
+            lastErrorMessage="Unrecognized parking ticket.";
+            return null;
+        }
+        Car fetchedCar=parkingLot.getCar(ticket);
+        if(fetchedCar==null) {
+            lastErrorMessage="Unrecognized parking ticket.";
+        }
+        return fetchedCar;
+    }
+
+    public String getLastErrorMessage() {
+        return lastErrorMessage;
     }
 }
